27a05259ab920bd27de28bddd51eca7c7adf805a
Wojtek Okoński <wojteko22.96@gmail.com>
2018-02-15 13:45:58 +0100

    Fix lambda

--- src/main/java/gLambda.kt
+++ src/main/java/gLambda.kt
@@ -4,6 +4,15 @@ val sum2 = { x: Int, y: Int -> x + y }
 fun main(args: Array<String>) {
     println("sum1: ${sum(1, 2)}, sum2: ${sum2(1, 2)}")
 
-    val filteredCollection = listOf(1, 2, 3, 4).filter({ it > 2 }) // todo: nawiasy
-    print(filteredCollection)
+    val filteredCollection1 = listOf(1, 2, 3, 4).filter({ element -> element > 2 })
+    val filteredCollection2 = listOf(1, 2, 3, 4).filter({ it > 2 })
+    val filteredCollection3 = listOf(1, 2, 3, 4).filter { it > 2 }
+    val filteredCollection4 = listOf(1, 2, 3, 4).filter(::greaterThanTwo)
+
+    println(filteredCollection1)
+    println(filteredCollection2)
+    println(filteredCollection3)
+    println(filteredCollection4)
 }
+
+fun greaterThanTwo(number: Int) = number > 2
